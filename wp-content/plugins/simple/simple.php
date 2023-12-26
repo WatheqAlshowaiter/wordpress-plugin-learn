f226363d48ca9fc7abdec20ec617a5c58688d7d4
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2023-12-27 00:16:48 +0300

    complete #10

--- wp-content/plugins/simple/simple.php
+++ wp-content/plugins/simple/simple.php
@@ -57,3 +57,32 @@ function my_plugin_on_uninstall()
 }
 
 register_activation_hook(__FILE__, 'my_plugin_on_uninstall');
+
+
+// customize
+function custom_logout()
+{
+    // something..
+}
+add_action('wp_logout', 'custom_logout');
+
+// or..
+
+function wp_logout()
+{
+    $user_id = get_current_user_id();
+
+    wp_destroy_current_session();
+    wp_clear_auth_cookie();
+    wp_set_current_user(0);
+
+    /**
+     * Fires after a user is logged out.
+     *
+     * @since 1.5.0
+     * @since 5.5.0 Added the `$user_id` parameter.
+     *
+     * @param int $user_id ID of the user that was logged out.
+     */
+    do_action('wp_logout', $user_id);
+}
