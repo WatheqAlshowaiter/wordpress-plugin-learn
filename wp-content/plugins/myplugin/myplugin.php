d7539e6795e4e473f51a4fe144f7a2b609dd169c
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2023-12-27 23:10:42 +0300

    complete #13

---
+++ wp-content/plugins/myplugin/myplugin.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://iwatheq.com
+ * @since             1.0.0
+ * @package           Myplugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       myplugin
+ * Plugin URI:        https://iwatheq.com
+ * Description:       Learn to build plugins..
+ * Version:           1.0.0
+ * Author:            Watheq
+ * Author URI:        https://iwatheq.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       myplugin
+ * Domain Path:       /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
